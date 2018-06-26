43592388ca58b78d3edf95f671764820af271c74
b0nz <elliantosarono.p@gmail.com>
2018-06-26 15:51:40 +0700

    add gitignore

--- curriculum/api/views.py
+++ curriculum/api/views.py
@@ -6,8 +6,8 @@ class CurriculumViewSet(viewsets.ModelViewSet):
     serializer_class = CurriculumSerializers
     queryset = Curriculum.objects.all()
 
-    def create(self, request, *args, **kwargs,):
-       serializer = self.get_serializer(data=request.data)
-       serializer.is_valid(raise_exception=True)
-       serializer.save(created_by=request.user,updated_by=request.user)
-       return Response(serializer.data, status=status.HTTP_201_CREATED) 
+    # def create(self, request, *args, **kwargs,):
+    #    serializer = self.get_serializer(data=request.data)
+    #    serializer.is_valid(raise_exception=True)
+    #    serializer.save(created_by=request.user,updated_by=request.user)
+    #    return Response(serializer.data, status=status.HTTP_201_CREATED) 
