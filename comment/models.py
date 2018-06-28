dd367283291561a8d3420f7f1a704898cf4803d6
jajangratis <trianganteng1234@gmail.com>
2018-06-28 21:07:06 +0700

    add comment manyToone lecture

--- comment/models.py
+++ comment/models.py
@@ -28,7 +28,7 @@ class Comment(models.Model):
     lecture = models.ForeignKey(
         "lecture.Lecture",
         on_delete = models.SET_NULL,
-        related_name = "to_lecture",
+        related_name = "from_comment",
         null = True,
     )
 
